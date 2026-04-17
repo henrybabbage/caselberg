7b1a59caef40a6d95d50e0a1cab638552cd6805a
Henry Babbage <hbabbage@gmail.com>
2026-04-17 03:51:56 +0200

    update gsap

--- web/src/lib/gsap.ts
+++ web/src/lib/gsap.ts
@@ -1,6 +1,3 @@
-'use client'
-
-import {useGSAP} from '@gsap/react'
 import gsap from 'gsap'
 import {CustomEase} from 'gsap/CustomEase'
 import {Draggable} from 'gsap/Draggable'
@@ -9,11 +6,11 @@ import {Observer} from 'gsap/Observer'
 import {ScrollTrigger} from 'gsap/ScrollTrigger'
 import {SplitText} from 'gsap/SplitText'
 
-gsap.registerPlugin(useGSAP, Flip, SplitText, Draggable, ScrollTrigger, Observer, CustomEase)
+gsap.registerPlugin(Flip, SplitText, Draggable, ScrollTrigger, Observer, CustomEase)
 
 gsap.config({
   autoSleep: 60,
   nullTargetWarn: false,
 })
 
-export {CustomEase, Draggable, Flip, gsap, Observer, ScrollTrigger, SplitText, useGSAP}
+export {CustomEase, Draggable, Flip, gsap, Observer, ScrollTrigger, SplitText}
