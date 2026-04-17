e6ccd81806376dba05bf8527cbcd993a5905e411
Henry Babbage <hbabbage@gmail.com>
2026-04-17 04:31:56 +0200

    amend gsap file

--- web/src/lib/gsap.ts
+++ web/src/lib/gsap.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/environment'
 import gsap from 'gsap'
 import {CustomEase} from 'gsap/CustomEase'
 import {Draggable} from 'gsap/Draggable'
@@ -5,11 +6,13 @@ import {Flip} from 'gsap/Flip'
 import {Observer} from 'gsap/Observer'
 import {ScrollTrigger} from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(Flip, Draggable, ScrollTrigger, Observer, CustomEase)
+if (browser) {
+  gsap.registerPlugin(Flip, Draggable, ScrollTrigger, Observer, CustomEase)
 
-gsap.config({
-  autoSleep: 60,
-  nullTargetWarn: false,
-})
+  gsap.config({
+    autoSleep: 60,
+    nullTargetWarn: false,
+  })
+}
 
 export {CustomEase, Draggable, Flip, gsap, Observer, ScrollTrigger}
